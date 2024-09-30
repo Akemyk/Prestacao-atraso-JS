99734d009fc8e9629186d8b40d7b4cf0a8e9d237
Julia Akemy <144185022+Akemyk@users.noreply.github.com>
2024-09-30 19:25:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let valor = Number(prompt("Digite o valor da prestação em atraso"))
+let taxa = Number(prompt("Digite o valor da taxa"))
+let tempo = Number(prompt("Digite o tempo"))
+let prestacao = valor+(valor*taxa/100)*tempo
